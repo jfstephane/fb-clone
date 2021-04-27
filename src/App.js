0cd61e02cf6bbcbf4d061e3c8c219e0b5711c720
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2021-04-27 19:08:10 -0400

    SideBar complete
    Add sidebarrow with icon and texts with hover effecr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
-import Header from "./Header"
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
     <div className="app">
 
 <Header/>
+
+<div className="app__body">
+
+<Sidebar/>
+
+</div>
 
 
 
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import "./Sidebar.css"
+import SidebarRow from "./SidebarRow"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags"
+import PeopleIcon from "@material-ui/icons/People"
+import ChatIcon from "@material-ui/icons/Chat"
+import StorefrontIcon from "@material-ui/icons/Storefront"
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+
+            <SidebarRow 
+            src='https://upload.wikimedia.org/wikipedia/commons/3/35/Pisitif_logo_2018_%28French_film_magazine%29.png'
+            title="FFFFFFFFFFF"/>
+
+            <SidebarRow 
+            Icon={EmojiFlagsIcon}
+            title="Pages"/>
+            <SidebarRow 
+            Icon={PeopleIcon}
+            title="Friends"/>
+            <SidebarRow
+            Icon={ChatIcon}
+            title="Messenger"/>
+            <SidebarRow
+            Icon={StorefrontIcon}
+            title="MarketPlace"/>
+            <SidebarRow
+            Icon={VideoLibraryIcon}
+            title="Videos"
+            />
+            <SidebarRow
+            Icon={ExpandMoreIcon}
+            title="MarketPlace"
+            />
+        </div>
+    )
+}
+
+export default Sidebar
