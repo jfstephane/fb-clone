e0c10f06f69f5f1feba4d276c422d93f03bcdad8
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2021-04-27 17:24:50 -0400

    Header done
    first Commit
    Header complete with logo search view, icons

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChV-3CubFpvV1X16CkLSbHPBU7T34zGE4",
+    authDomain: "pozitif-77e2d.firebaseapp.com",
+    projectId: "pozitif-77e2d",
+    storageBucket: "pozitif-77e2d.appspot.com",
+    messagingSenderId: "399849643761",
+    appId: "1:399849643761:web:72a5485c26e637f2df9350",
+    measurementId: "G-C42LHE9YZF"
+  };
