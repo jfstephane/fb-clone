50f1f0961e2fdea8dba68abcd609f07f6b422a99
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2021-04-28 13:00:09 -0400

    StoryPart
    story part done with background image bug (not showing)

---
+++ src/StoryReel.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Story from './Story';
+import"./StoryReel.css";
+
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+ <Story
+            image="https://c8.alamy.com/comp/PMC6F7/abstraction-orange-bokeh-on-a-blue-background-defocused-free-space-for-text-PMC6F7.jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+<Story
+            image="https://img.freepik.com/photos-gratuite/fond-blocs-voronoi-extrude-bleu-abstrait-mur-entreprise-minimal-leger-propre-illustration-surface-geometrique-3d-deplacement-elements-polygonaux_1217-2510.jpg?size=626&ext=jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+<Story
+            image="https://img.freepik.com/photos-gratuite/fond-blocs-voronoi-extrude-bleu-abstrait-mur-entreprise-minimal-leger-propre-illustration-surface-geometrique-3d-deplacement-elements-polygonaux_1217-2510.jpg?size=626&ext=jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+<Story
+            image="https://img.freepik.com/photos-gratuite/fond-blocs-voronoi-extrude-bleu-abstrait-mur-entreprise-minimal-leger-propre-illustration-surface-geometrique-3d-deplacement-elements-polygonaux_1217-2510.jpg?size=626&ext=jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+<Story
+            image="https://img.freepik.com/photos-gratuite/fond-blocs-voronoi-extrude-bleu-abstrait-mur-entreprise-minimal-leger-propre-illustration-surface-geometrique-3d-deplacement-elements-polygonaux_1217-2510.jpg?size=626&ext=jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+<Story
+            image="https://img.freepik.com/photos-gratuite/fond-blocs-voronoi-extrude-bleu-abstrait-mur-entreprise-minimal-leger-propre-illustration-surface-geometrique-3d-deplacement-elements-polygonaux_1217-2510.jpg?size=626&ext=jpg"
+            profileSrc="https://img.freepik.com/vecteurs-libre/abstrait-bleu-formes-geometriques_1035-17545.jpg?size=626&ext=jpg"
+            title="Test"/>
+
+
+        </div>
+    )
+}
+
+export default StoryReel
